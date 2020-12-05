86058f328e4077c5f421c1405a32500a9496a13c
Claire Hough <claire.n.hough@gmail.com>
2020-12-05 16:46:07 -0600

    removed hardcoded values in functions

--- backend.py
+++ backend.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import sys
 #from PIL import Image
 
-def gaussian_filter(img):
-    gauss_img = cv2.GaussianBlur(img,(5,5),0)
+def gaussian_filter(img, sigma):
+    gauss_img = cv2.GaussianBlur(img,(5,5),sigma)
     return gauss_img
 
 def img_resize(img, size):
@@ -16,13 +16,13 @@ def img_resize(img, size):
     resized_img = cv2.resize(img, dim)
     return resized_img
 
-def gray_to_color(img):
+def gray_to_color(img, red, green, blue):
     # blue
-    img[:,:,0] = (img[:,:,0] * 0.59).astype(int)
+    img[:,:,0] = (img[:,:,0] * blue).astype(int)
     # green
-    img[:,:,1] = (img[:,:,1] * 0.11).astype(int)
+    img[:,:,1] = (img[:,:,1] * green).astype(int)
     #red
-    img[:,:,2] = (img[:,:,2] * 0.5).astype(int)
+    img[:,:,2] = (img[:,:,2] * red).astype(int)
     return img
 
 
@@ -33,25 +33,17 @@ cv2.imshow('Original Image', current_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-new_image = gaussian_filter(current_image)
-#compare1 = np.concatenate((current_image, new_image), axis =1)
+new_image = gaussian_filter(current_image, 10)
 cv2.imshow('Gaussian Filtering', new_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-new_image = img_resize(current_image, 60)
-#compare1 = np.concatenate((current_image, new_image), axis =1)
-cv2.imshow('Image Resize', new_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-new_image = gray_to_color(current_image)
-#compare1 = np.concatenate((current_image, new_image), axis =1)
-#cv2.imshow('Gray To Color', new_image)
-#plt.axis("off")
-#plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+new_image = gray_to_color(current_image, 0.6, 1, 0.33)
 cv2.imshow('Gray to Color', new_image)
-#plt.show()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+new_image = gray_to_color(current_image, 1.8, 0.7, 1.8)
+cv2.imshow('Gray to Color', new_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
