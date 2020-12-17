25fe65c033105d63272c6eab18f0411261b0950e
Claire Hough <claire.n.hough@gmail.com>
2020-12-17 02:51:48 -0600

    everything working

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
+import cv2
+import time
+# import numpy as np
+# import matplotlib.pyplot as plt
 import sys
+from backend import *
 
 Ui_Dialog, QDialog = loadUiType("app.ui")
 
@@ -13,11 +18,18 @@ class App(QDialog, Ui_Dialog):
         self.setupUi(self)
         self.imageBox.setScaledContents(True)
 
-        self.image = QPixmap('yourImg.jpg')
+        #self.image = QPixmap('yourImg.jpg')
+        self.img = cv2.imread('yourImg.jpg')
         self.imgFileName = 'yourImg.jpg'
         self.placeholderFileName = 'yourImg.jpg'
-        self.placeholder = QPixmap('yourImg.jpg')
-        self.imageBox.setPixmap(self.placeholder)
+        #self.placeholder = QPixmap('yourImg.jpg')
+        self.placeholder = cv2.imread('yourImg.jpg')
+
+        height, width, channel = self.placeholder.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(self.placeholder.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+
+        self.imageBox.setPixmap(QPixmap(qImg))
 
         self.setWindowTitle("Insta Photo Editor")
         self.timer = QTimer()
@@ -26,10 +38,13 @@ class App(QDialog, Ui_Dialog):
         self.importButton.clicked.connect(self.getFile)
         self.exportButton.clicked.connect(self.saveFile)
         self.redSlide.valueChanged.connect(self.redChanged)
+        self.redSlide.setMaximum(200)
         self.greenSlide.valueChanged.connect(self.greenChanged)
+        self.greenSlide.setMaximum(200)
         self.blueSlide.valueChanged.connect(self.blueChanged)
-        self.sharpSlide.valueChanged.connect(self.sharpChanged)
-        self.contrastSlide.valueChanged.connect(self.contrastChanged)
+        self.blueSlide.setMaximum(200)
+        self.sharpSlide.toggled.connect(self.sharpChanged)
+        self.blurSlide.toggled.connect(self.blurChanged)
         self.rgbRadio.toggled.connect(self.rgbChecked)
         self.grayscaleRadio.toggled.connect(self.grayscaleChecked)
         self.sizeBox.currentIndexChanged.connect(self.sizeChanged)
@@ -39,8 +54,7 @@ class App(QDialog, Ui_Dialog):
             'c:\\',"Image files (*.jpg *.gif)")
         
         if fname != "":
-            pixmap = QPixmap(fname)
-            self.updateImg(pixmap)
+            self.updateImg(fname)
             self.imgFileName = fname
         else:
             self.errorSuccess.setStyleSheet("color: red; font: 12pt; MS Shell Dlg;")
@@ -62,38 +76,88 @@ class App(QDialog, Ui_Dialog):
             self.timer.timeout.connect(self.onTimeout)
             self.timer.start(5000)
         else:
-            self.image.save(fname)
+            height, width, channel = self.image.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(self.image.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+            toSave = QPixmap(qImg)
+            toSave.save(fname)
             self.errorSuccess.setStyleSheet("color: green; font: 12pt; MS Shell Dlg;")
             self.errorSuccess.setText("Successfully saved!")
             self.timer.timeout.connect(self.onTimeout)
             self.timer.start(5000)
 
-    def updateImg(self, pixmap):
-        self.image = pixmap
-        self.imageBox.setPixmap(pixmap)
+    def updateImg(self, fname):
+        self.image = cv2.imread(fname)
 
-    def redChanged(self):
-        print("red")
+        height, width, channel = self.image.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(self.image.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        
+        self.imageBox.setPixmap(QPixmap(qImg))
 
-    def greenChanged(self):
-        print("green")
+    def updateImg2(self, imagename):
+        height, width, channel = self.image.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(self.image.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        
+        self.imageBox.setPixmap(QPixmap(qImg))
 
-    def blueChanged(self):
-        print("blue")
+    def redChanged(self, value):
+        self.updateImg(self.imgFileName)
+        img = change_red(self.image, value)
+        self.updateImg2(self.image)
+
+    def greenChanged(self, value):
+        self.updateImg(self.imgFileName)
+        img = change_green(self.image, value)
+        self.updateImg2(self.image)
+
+    def blueChanged(self, value):
+        self.updateImg(self.imgFileName)
+        img = change_blue(self.image, value)
+        self.updateImg2(self.image)
 
     def sharpChanged(self):
-        print("sharp")
+        # self.updateImg(self.imgFileName)
+        # img = sharpChanged(self.image, value)
+        # self.updateImg2(self.image)
 
-    def contrastChanged(self):
-        print("contrast")
+        cv2.imshow('Original Image', self.image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        self.updateImg(self.imgFileName)
+        img = sharpChanged(self.image)
+
+        cv2.imshow('Sharpened Image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        self.updateImg2(img)
+
+    def blurChanged(self, value):
+        cv2.imshow('Original Image', self.image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        self.updateImg(self.imgFileName)
+        img = cv2.GaussianBlur(self.image,(5,5),100)
+
+        cv2.imshow('Blurred Image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        self.updateImg2(img)
 
     def rgbChecked(self):
         if self.rgbRadio.isChecked():
             print("rgb")
 
     def grayscaleChecked(self):
-        if self.grayscaleRadio.isChecked():
-            print("grayscale")
+        # if self.grayscaleRadio.isChecked():
+        img = grayConversion(self.image)
+        self.updateImg2(img)
+        print("grayscale")
 
     def sizeChanged(self):
         print(self.sizeBox.currentText())
--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-#from PIL import Image
+from PIL import ImageEnhance
 
 def gaussian_filter(img, sigma):
     gauss_img = cv2.GaussianBlur(img,(5,5),sigma)
@@ -16,34 +16,67 @@ def img_resize(img, size):
     resized_img = cv2.resize(img, dim)
     return resized_img
 
-def gray_to_color(img, red, green, blue):
-    # blue
-    img[:,:,0] = (img[:,:,0] * blue).astype(int)
-    # green
-    img[:,:,1] = (img[:,:,1] * green).astype(int)
-    #red
-    img[:,:,2] = (img[:,:,2] * red).astype(int)
+def change_red(img, red):
+    img[:,:,2] = (img[:,:,2] * (red/100)).astype(int)
     return img
 
+def change_green(img, green):
+    img[:,:,1] = (img[:,:,1] * (green/100)).astype(int)
+    return img
 
+def change_blue(img, blue):
+    img[:,:,0] = (img[:,:,0] * (blue/100)).astype(int)
+    return img
 
+def grayConversion(img):
+    grayValue = (0.07 * img[:,:,2]).astype(int) + (0.72 * img[:,:,1]).astype(int) + (0.21 * img[:,:,0]).astype(int)
+    img[:,:,0] = grayValue
+    img[:,:,1] = grayValue
+    img[:,:,2] = grayValue
+    return img
 
-current_image = cv2.imread("Fig1016(a)(building_original).tif")
-cv2.imshow('Original Image', current_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def histogram_equalization(image):
+    equ = cv2.equalizeHist(image)
+    res = np.hstack((image,equ)) #stacking images side-by-side
+   
 
-new_image = gaussian_filter(current_image, 10)
-cv2.imshow('Gaussian Filtering', new_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def sharpChanged(image):
+    #factor = self.sharpSlide.value()
+    # if(factor <= 3):
+    #     sharp_kernel = np.array([[-1,-1,-1], [1,7,-1], [-1,-1,-1]])
+    
+    # if(factor >= 4 or factor <= 8):
+    sharp_kernel = np.array([[-1,-1,-1], [1,7,-1], [-1,-1,-1]])
+   # sharp_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]) 
+    
+    # if(factor >=8 ):
+    #     sharp_kernel = np.array([[1,1,1], [-1,-3,-1], [1,1,1]])
+    #   #  sharp_kernel = np.array([[1,1,1], [1,-7,1], [1,1,1]])
 
-new_image = gray_to_color(current_image, 0.6, 1, 0.33)
-cv2.imshow('Gray to Color', new_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    sharpen = cv2.filter2D(image, -1, sharp_kernel)   
+    return sharpen
 
-new_image = gray_to_color(current_image, 1.8, 0.7, 1.8)
-cv2.imshow('Gray to Color', new_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# current_image = cv2.imread("Fig1016(a)(building_original).tif")
+# cv2.imshow('Original Image', current_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# new_image = gaussian_filter(current_image, 10)
+# cv2.imshow('Gaussian Filtering', new_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# new_image = gray_to_color(current_image, 0.6, 1, 0.33)
+# cv2.imshow('Gray to Color', new_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# new_image = gray_to_color(current_image, 1.8, 0.7, 1.8)
+# cv2.imshow('Gray to Color', new_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# new_image = grayConversion(current_image)
+# cv2.imshow('Color To Gray', new_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
